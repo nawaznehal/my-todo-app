a96de67eda9434f61dcf39d07c5e41f186a97037
Nawaz <nawaznehal786@gmail.com>
2024-11-20 02:26:55 -0500

    add 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,7 +10,7 @@ const NotFoundPage = () => {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-red-500">404 - Page Not Found</h1>
-      <p className="text-lg text-gray-700 mt-4">Oops! The page you're looking for doesn't exist.</p>
+      <p className="text-lg text-gray-700 mt-4">Oops! The page you are looking for does not exist.</p>
       <button
         onClick={handleGoHome}
         className="mt-6 px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
