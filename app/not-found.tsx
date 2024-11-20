89e42995e07309060e8026cac2c2ec35add33dd5
nawaznehal <50049028+nawaznehal@users.noreply.github.com>
2024-11-20 15:55:18 -0500

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link';
-
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
